e3883f70db596bf0b5de50b881d4e76c2257f554
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-06-11 15:24:19 +0800

    feat: add session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const users = require('./users.json');
 
 const app = express()
@@ -9,7 +10,16 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 
+app.use(session({
+  secret: 'Secret',
+  name: 'user', // optional
+  saveUninitialized: false,
+  resave: true, 
+}))
+
 app.get('/', (req, res) =>{
+  console.log("session",req.session)
+  console.log("sessionID",req.sessionID) 
   res.render('index')
 })
 
@@ -28,12 +38,12 @@ app.post('/login', (req, res) => {
     //這個抓取方式要記得
     const passwordexist = Emailexist.password
     
-
     if( passwordexist!== Password){
 
       res.render('index' , {  Emailunexist : Email })
     } else {
       const username = Emailexist.firstName
+      req.session.user = username
       res.render('welcome' ,{  username : username })
     }
    
